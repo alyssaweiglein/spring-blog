69439cf6ae8ecea117f62b2bae2b116cafb5fce5
Alyssa Weiglein <contact@arw.works>
2022-11-08 19:02:35 -0800

    blog posts

--- src/main/java/com/example/springblog/config/SeedData.java
+++ src/main/java/com/example/springblog/config/SeedData.java
@@ -24,8 +24,8 @@ public class SeedData implements CommandLineRunner {
             post1.setBody("Body (Post 1)");
 
             Post post2 = new Post();
-            post1.setTitle("Title (Post 2)");
-            post1.setBody("Body (Post 2)");
+            post2.setTitle("Title (Post 2)");
+            post2.setBody("Body (Post 2)");
 
             postService.save(post1);
             postService.save(post2);
