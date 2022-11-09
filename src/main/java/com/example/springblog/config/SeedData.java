344913bec35582bd7b729ae9692dfa75b86411ca
Alyssa Weiglein <contact@arw.works>
2022-11-08 16:12:06 -0800

    first commit

---
+++ src/main/java/com/example/springblog/config/SeedData.java
@@ -0,0 +1,34 @@
+package com.example.springblog.config;
+
+import com.example.springblog.models.Post;
+import com.example.springblog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SeedData implements CommandLineRunner {
+
+    @Autowired
+    private PostService postService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Post> posts = postService.getAll();
+
+        if (posts.size() == 0) {
+            Post post1 = new Post();
+            post1.setTitle("Title (Post 1)");
+            post1.setBody("Body (Post 1)");
+
+            Post post2 = new Post();
+            post1.setTitle("Title (Post 2)");
+            post1.setBody("Body (Post 2)");
+
+            postService.save(post1);
+            postService.save(post2);
+        }
+    }
+}
