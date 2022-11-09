7e89fa2d2fb84ae3dd0074258bc0e17cb18a62fd
Alyssa Weiglein <contact@arw.works>
2022-11-09 13:37:11 -0800

    LoginController, RegisterController, post_new

--- src/main/java/com/example/springblog/controller/PostController.java
+++ src/main/java/com/example/springblog/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.example.springblog.controller;
 
+import com.example.springblog.models.Account;
 import com.example.springblog.models.Post;
+import com.example.springblog.services.AccountService;
 import com.example.springblog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,9 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
         Optional<Post> optionalPost = postService.getById(id);
@@ -22,6 +27,19 @@ public class PostController {
             Post post = optionalPost.get();
             model.addAttribute("post", post);
             return "post";
+        } else {
+            return "404";
+        }
+    }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model) {
+        Optional<Account> optionalAccount = accountService.findByEmail("user.user@domain.com");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_new";
         } else {
             return "404";
         }
--- src/main/java/com/example/springblog/services/AccountService.java
+++ src/main/java/com/example/springblog/services/AccountService.java
@@ -5,6 +5,8 @@ import com.example.springblog.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountService {
     @Autowired
@@ -12,5 +14,9 @@ public class AccountService {
 
     public Account save(Account account) {
         return accountRepository.save(account);
+    }
+
+    public Optional<Account> findByEmail(String email) {
+        return accountRepository.findOneByEmail(email);
     }
 }
